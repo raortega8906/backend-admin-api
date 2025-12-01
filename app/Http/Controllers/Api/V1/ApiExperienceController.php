9d9bd1f2ad92d2060f533549de58b6047ce75b2e
rortega8906 <raortega8906@gmail.com>
2025-12-02 00:38:09 +0100

    feat: api experience

--- app/Http/Controllers/Api/V1/ApiExperienceController.php
+++ app/Http/Controllers/Api/V1/ApiExperienceController.php
@@ -3,9 +3,44 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Experience;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ApiExperienceController extends Controller
 {
-    //
+    public function index (): JsonResponse
+    {
+        $data = Experience::orderBy('start_year', 'desc')->get();
+
+        if ($data->isEmpty()) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No hay experiencia laboral registrada',
+                'data' => $data
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Listado de la experiencia laboral',
+            'data' => $data
+        ], 200);
+    }
+
+    public function show(Experience $experience): JsonResponse
+    {
+        if (!$experience) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No se pudo encontrar la experiencia laboral',
+                'data' => []
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Experiencia laboral encontrada',
+            'data' => $experience
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\V1\ApiExperienceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+// Experiences:
+Route::prefix('v1')->group(function() {
+
+    Route::get('experiences', [ApiExperienceController::class, 'index']);
+    Route::get('experiences/{experience}', [ApiExperienceController::class, 'show']);
+
+});
