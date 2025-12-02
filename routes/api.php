35014dc794afc8d3d87b53e6d1cea83cf3814461
rortega8906 <raortega8906@gmail.com>
2025-12-02 17:03:45 +0100

    feat: api projects

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\ApiExperienceController;
+use App\Http\Controllers\Api\V1\ApiProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,11 +9,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-// Experiences:
 Route::prefix('v1')->group(function() {
 
+    // Experiences:
     Route::get('experiences', [ApiExperienceController::class, 'index']);
     Route::get('experiences/{experience}', [ApiExperienceController::class, 'show']);
+
+    // Projects:
+    Route::get('projects', [ApiProjectController::class, 'index']);
+    Route::get('projects/project', [ApiProjectController::class, 'show']);
 
 });
